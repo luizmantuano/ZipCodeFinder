99d23a108ef3aed74f4c84e8bff36d5352eb4b3b
luizmantuano <luguto23@hotmail.com>
2024-04-03 13:42:13 -0300

    feat: 'applying responsiveness to the project and placing an animated icon'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import { Search } from 'lucide-react';
 import { useState } from 'react';
 import Api from '../service/api';
+import Lottie from 'react-lottie';
+import animationData from '@/assets/Animation.json'
 
 export interface ApiResponse {
   logradouro: string;
@@ -43,21 +45,36 @@ export default function Home() {
     }
   }
 
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
   return (
     <main className='flex flex-col h-screen w-full items-center justify-center bg-gradient-to-r from-cyan-300 to-blue-500'>
-      <div className='flex flex-col items-center w-[550px] h-[350px] shadow-2xl bg-white rounded-3xl'>
-        <div className='mt-6'>
-          <h1 className='text-[30px] line-clamp-1 text-center font-bold font-sans'>
+      <div className='flex flex-row items-center justify-center md:mt-[50px]'>
+          <h1 className='text-[30px] mt-[-100px] text-white line-clamp-1 text-center font-bold font-sans md:text-[40px]'>
             Buscador de Cep
           </h1>
-        </div>
-        <div className='flex flex-row items-center justify-center mt-6 w-[200px]'>
+          <span className='mt-[-100px] ml-8'>
+              <Lottie options={defaultOptions}
+              height={100}
+              width={100}
+              />
+          </span>
+      </div>
+      <div className='flex flex-col items-center w-[400px] h-96 shadow-2xl bg-white rounded-3xl mt-[20px] md:w-[750px] md:h-[400px]'>
+        <div className='flex flex-row items-center justify-center mt-[60px] w-[200px]'>
           <label className='relative block '>
             <span className='absolute inset-y-0 left-0 flex items-center pl-2'>
               <Search className='w-[15px]' />
             </span>
             <input
-              className='placeholder:italic placeholder:text-slate-400 block bg-white w-[200px] border border-slate-300 rounded-md h-9 pl-9 shadow-sm focus:outline-none focus:border-sky-500  text-[13px] font-bold'
+              className='placeholder:italic placeholder:text-slate-400 block bg-white w-[200px] border border-slate-300 rounded-md h-9 pl-9 shadow-sm focus:outline-none focus:border-sky-500  text-[13px] font-bold '
               placeholder='Número do CEP'
               type='text'
               name='search'
@@ -72,20 +89,20 @@ export default function Home() {
           </button>
         </div>
         {showResults && (
-          <div className='flex flex-col space-y-3 mt-10'>
-            <div className='flex items-center justify-center border border-slate-400 w-[200px] h-7 rounded-full'>
-              <span className='text-[12px] font-bold'>{logradouro}</span>
-            </div>
-            <div className='flex items-center justify-center border border-slate-400 w-[200px] h-7 rounded-full'>
-              <span className='text-[12px] font-bold'>{bairro}</span>
-            </div>
-            <div className='flex items-center justify-center border border-slate-400 w-[200px] h-7 rounded-full'>
-              <span className='text-[12px] font-bold'>{localidade}</span>
-            </div>
-            <div className='flex items-center justify-center border border-slate-400 w-[200px] h-7 rounded-full'>
-              <span className='text-[12px] font-bold'>{uf}</span>
-            </div>
-          </div>
+          <ul className='flex flex-col space-y-3 mt-10'>
+            <li className='flex items-center justify-center border border-slate-400 w-[300px] h-10 rounded-full md:w-[500px]'>
+              <span className='text-[12px] font-bold md:text-[18px]'>{logradouro}</span>
+            </li>
+            <li className='flex items-center justify-center border border-slate-400 w-[300px] h-10 rounded-full md:w-[500px]'>
+              <span className='text-[12px] font-bold md:text-[18px]'>{bairro}</span>
+            </li>
+            <li className='flex items-center justify-center border border-slate-400 w-[300px] h-10 rounded-full md:w-[500px]'>
+              <span className='text-[12px] font-bold md:text-[18px]'>{localidade}</span>
+            </li>
+            <li className='flex items-center justify-center border border-slate-400 w-[300px] h-10 rounded-full md:w-[500px]'>
+              <span className='text-[12px] font-bold md:text-[18px]'>{uf}</span>
+            </li>
+          </ul>
         )}
       </div>
     </main>
